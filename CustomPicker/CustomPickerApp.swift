3515bd92ac1a08240568b6d8d34f8aa1855c8560
pouyasadri <pouyapaghandeh78@gmail.com>
2024-04-29 13:43:22 +0200

    Initial Commit

---
+++ CustomPicker/CustomPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomPickerApp.swift
+//  CustomPicker
+//
+//  Created by Pouya Sadri on 29/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
